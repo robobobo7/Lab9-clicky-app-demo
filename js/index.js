1cd89e226777ea5e75119c81af255257555f49d2
Scotty F <scottymferreira@gmail.com>
2017-11-19 17:14:08 +0000

    initial commit

---
+++ js/index.js
@@ -0,0 +1,515 @@
+"use strict";
+
+var _templateObject = _taggedTemplateLiteralLoose(["\n  padding: 0 ", ";\n  display: flex;\n  flex-direction: row;\n  width: 100%;\n  justify-content: ", ";\n\n  @media (max-width: 700px) {\n\t\tflex-direction: ", ";\n\t}\n"], ["\n  padding: 0 ", ";\n  display: flex;\n  flex-direction: row;\n  width: 100%;\n  justify-content: ", ";\n\n  @media (max-width: 700px) {\n\t\tflex-direction: ", ";\n\t}\n"]),
+    _templateObject2 = _taggedTemplateLiteralLoose(["\n  padding: ", " 0;\n  display: flex;\n  flex-direction: column;\n  flex: 1;\n  max-width: ", ";\n"], ["\n  padding: ", " 0;\n  display: flex;\n  flex-direction: column;\n  flex: 1;\n  max-width: ", ";\n"]),
+    _templateObject3 = _taggedTemplateLiteralLoose(["\n  justify-content: center;\n  width:100%;\n  align-items: center;\n  padding: 2em;\n"], ["\n  justify-content: center;\n  width:100%;\n  align-items: center;\n  padding: 2em;\n"]),
+    _templateObject4 = _taggedTemplateLiteralLoose(["\n box-shadow:  0 1px 3px 0 rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 2px 1px -1px rgba(0,0,0,.12);\n  justify-content: center;\n  align-items: center;\n  margin: 20px;\n  padding: ", ";\n  margin-top: 50px;\n  background-color: #e1e2e1;\n"], ["\n box-shadow:  0 1px 3px 0 rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 2px 1px -1px rgba(0,0,0,.12);\n  justify-content: center;\n  align-items: center;\n  margin: 20px;\n  padding: ", ";\n  margin-top: 50px;\n  background-color: #e1e2e1;\n"]),
+    _templateObject5 = _taggedTemplateLiteralLoose(["\nwidth: 100%;\nmargin-bottom: 1em;\nborder: none;\nborder-bottom: 1px solid #d2d0d0;\n\n:focus{\n  border: none;\n  outline: none;\n  border-bottom: 2px solid ", ";\n  border-right: 2px solid ", ";\n}\n"], ["\nwidth: 100%;\nmargin-bottom: 1em;\nborder: none;\nborder-bottom: 1px solid #d2d0d0;\n\n:focus{\n  border: none;\n  outline: none;\n  border-bottom: 2px solid ", ";\n  border-right: 2px solid ", ";\n}\n"]),
+    _templateObject6 = _taggedTemplateLiteralLoose(["\ndisplay: flex;\nflex-direction: column;\nwidth: 300px;\npadding: 15px;\n"], ["\ndisplay: flex;\nflex-direction: column;\nwidth: 300px;\npadding: 15px;\n"]),
+    _templateObject7 = _taggedTemplateLiteralLoose(["\nmargin-top: 10px;\ntext-align: left;\npadding: 0xp 15px;\n"], ["\nmargin-top: 10px;\ntext-align: left;\npadding: 0xp 15px;\n"]),
+    _templateObject8 = _taggedTemplateLiteralLoose(["\nbackground: ", ";\nwidth: 100%;\nbox-shadow:  0 1px 3px 0 rgba(0,0,0,.5);\n\n"], ["\nbackground: ", ";\nwidth: 100%;\nbox-shadow:  0 1px 3px 0 rgba(0,0,0,.5);\n\n"]),
+    _templateObject9 = _taggedTemplateLiteralLoose(["\nfont-weight: 500;\npadding: 20px;\ncolor: ", ";\n"], ["\nfont-weight: 500;\npadding: 20px;\ncolor: ", ";\n"]),
+    _templateObject10 = _taggedTemplateLiteralLoose(["\nfont-size: 8px;\nposition: absolute;\n"], ["\nfont-size: 8px;\nposition: absolute;\n"]),
+    _templateObject11 = _taggedTemplateLiteralLoose(["\nwidth: 46%;\nmax-width: 100px;\nbackground: #efefef;\nborder: none;\npadding: 8px;\nborder-radius: 2px; \ncolor: ", ";\ntext-transform: uppercase;\nfont-size: 12px;\noutline: none;\ncursor: pointer;\n  \n  :hover {\n    background-color: ", ";\n    color: white;\n  }\n"], ["\nwidth: 46%;\nmax-width: 100px;\nbackground: #efefef;\nborder: none;\npadding: 8px;\nborder-radius: 2px; \ncolor: ", ";\ntext-transform: uppercase;\nfont-size: 12px;\noutline: none;\ncursor: pointer;\n  \n  :hover {\n    background-color: ", ";\n    color: white;\n  }\n"]),
+    _templateObject12 = _taggedTemplateLiteralLoose(["\n  color:white;\n  max-width:150px;\n  border:none;\n  padding:10px;\n  border-radius:20px; \n  background: ", "\n  text-transform:uppercase;\n  font-size:.9em;\n  margin-bottom:25px;\n  cursor:pointer;\n\n  :hover {\n    background-color: #5c007a;\n  }\n"], ["\n  color:white;\n  max-width:150px;\n  border:none;\n  padding:10px;\n  border-radius:20px; \n  background: ", "\n  text-transform:uppercase;\n  font-size:.9em;\n  margin-bottom:25px;\n  cursor:pointer;\n\n  :hover {\n    background-color: #5c007a;\n  }\n"]);
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function _taggedTemplateLiteralLoose(strings, raw) { strings.raw = raw; return strings; }
+
+var config = {
+  apiKey: "AIzaSyD2B3Uqt_xVtwUS74pXGQbn0bVN4KPT03E",
+  authDomain: "pooptracker-79b4b.firebaseapp.com",
+  databaseURL: "https://pooptracker-79b4b.firebaseio.com",
+  projectId: "pooptracker-79b4b",
+  storageBucket: "pooptracker-79b4b.appspot.com",
+  messagingSenderId: "147343750066"
+};
+firebase.initializeApp(config);
+
+////local storage stuff
+var storageAvailable = function storageAvailable(type) {
+  try {
+    ////try to talk to the local storage area
+
+    var _storage = window[type],
+        x = '___storage_test___';
+    ///try putting x into the localStorage
+    _storage.setItem(x, x);
+    _storage.removeItem(x);
+    return true;
+  } catch (e) {
+    ///in case of failure
+    ///treat the error like any error in the browser
+    return e instanceof DOMException && (e.code === 22 || e.code === 1014 || e.name === 'QuotaExceededError' || e.name === 'NS_ERROR_COM_QUOTA_REACHED') && storage.length !== 0;
+  }
+};
+
+var poopsFromStorage = function poopsFromStorage() {
+  console.log('putting poop in storage');
+  if (storageAvailable('localStorage')) {
+    var pooptotal = window.localStorage.getItem('pooptotal');
+    return pooptotal || 0;
+  } else {
+    return 0;
+  }
+};
+
+var putPoopInStorage = function putPoopInStorage() {
+  var amount = arguments.length <= 0 || arguments[0] === undefined ? 1 : arguments[0];
+
+  console.log('getting coffee from storage');
+  if (storageAvailable('localStorage')) {
+    ///get the current total
+    var pooptotal = Number(window.localStorage.getItem('pooptotal')) || 0;
+    ///increase the current total
+    var newTotal = pooptotal + amount;
+    if (amount > 1) {
+      newTotal = amount;
+    }
+    window.localStorage.setItem('pooptotal', newTotal);
+  }
+};
+
+//Beginning of styled components
+var styled = styled.default;
+
+var PRIMARYCOLOR = {
+  COLOR: '#8e24aa',
+  SECONDARY: '#00e676',
+  TEXTDARK: '#000000',
+  TEXTLIGHT: '#ffffff',
+  TEXTALPHAMIN: '.59',
+  TEXTALPHAMAX: '1'
+};
+
+var Row = styled.div(_templateObject, function (props) {
+  return props.space ? props.space : '0';
+}, function (props) {
+  return props.rowJustify ? props.rowJustify : "center";
+}, function (props) {
+  return props.stack ? 'column' : 'row';
+});
+
+var Column = styled.div(_templateObject2, function (props) {
+  return props.space ? props.space : '0';
+}, function (props) {
+  return props.colMaxWidth ? props.colMaxWidth : "none";
+});
+var Container = Column.extend(_templateObject3);
+
+var Paper = Column.extend(_templateObject4, function (props) {
+  return props.paperPadding ? props.paperPadding : "20px";
+});
+
+var InputField = styled.input(_templateObject5, PRIMARYCOLOR.COLOR, PRIMARYCOLOR.COLOR);
+
+var Form = styled.form(_templateObject6);
+
+var FormLabel = styled.h4(_templateObject7);
+
+var MenuBar = styled.section(_templateObject8, PRIMARYCOLOR.COLOR);
+
+var MenuText = styled.h4(_templateObject9, PRIMARYCOLOR.TEXTLIGHT);
+
+var MouseText = styled.p(_templateObject10);
+
+var SubmitButton = styled.button(_templateObject11, PRIMARYCOLOR.TEXTCOLOR, PRIMARYCOLOR.COLOR);
+var StyledButton = styled.button(_templateObject12, PRIMARYCOLOR.SECONDARY);
+
+/////Main functionality
+
+var PoopData = function (_React$Component) {
+  _inherits(PoopData, _React$Component);
+
+  function PoopData() {
+    _classCallCheck(this, PoopData);
+
+    var _this = _possibleConstructorReturn(this, _React$Component.call(this));
+
+    _this.state = {
+      poopsHad: 1
+    };
+    return _this;
+  }
+
+  PoopData.prototype.componentWillMount = function componentWillMount() {
+    this.initializeData();
+    console.log("I'm about to be put on the screen!");
+    this.setState({ poopsHad: poopsFromStorage() }, function () {
+      console.log(this.state);
+    });
+  };
+
+  PoopData.prototype.initializeData = function initializeData() {
+    var _this2 = this;
+
+    firebase.database().ref('poops').on('value', function (snapshot) {
+      var dbTotal = snapshot.val().poopsHad || 0;
+      var localTotal = poopsFromStorage();
+      ///did we get some data?
+      if (dbTotal > localTotal) {
+        //update the local total
+        putPoopInStorage(dbTotal);
+      } else if (localTotal > dbTotal) {
+        ///update the database
+        firebase.database().ref('poops').set({ poopsHad: localTotal });
+      } else if (localTotal == dbTotal) {
+        console.log('You are synced to database!');
+      }
+
+      if (snapshot) {
+        _this2.setState(snapshot.val());
+      }
+    });
+  };
+
+  PoopData.prototype.takePoop = function takePoop() {
+    var _this3 = this;
+
+    ///update the local storage...
+    ///update the state
+    var temp = Number(this.state.poopsHad); ///Total or pooptotal?
+    this.setState({
+      poopsHad: temp + 1
+    }, function () {
+      ///put the data in the database
+      firebase.database().ref('poops').set({ poopsHad: _this3.state.poopsHad });
+      putPoopInStorage();
+    });
+  };
+
+  PoopData.prototype.render = function render() {
+    return React.createElement(
+      "div",
+      null,
+      React.createElement(UI, { total: this.state.poopsHad, drink: this.takePoop.bind(this) })
+    );
+  };
+
+  return PoopData;
+}(React.Component);
+
+var ProxiedButton = function ProxiedButton(props) {
+  var testMe = function testMe(event) {
+    props.action();
+  };
+  return React.createElement(
+    StyledButton,
+    { className: "poopButton", onClick: testMe },
+    props.label
+  ) //Something seems wrong here with the onClick...also, props.label?
+  ;
+};
+///First React Component
+var UI = function UI(props) {
+  return React.createElement(
+    Container,
+    null,
+    React.createElement(
+      Paper,
+      null,
+      React.createElement(
+        MenuBar,
+        null,
+        React.createElement(
+          Row,
+          null,
+          React.createElement(
+            MenuText,
+            null,
+            "Hello"
+          )
+        )
+      ),
+      React.createElement(
+        "h1",
+        null,
+        "Welcome Back ",
+        props.name
+      ),
+      React.createElement(
+        "h4",
+        null,
+        "Have you pooped today?"
+      ),
+      React.createElement(
+        "p",
+        null,
+        "Let's track some numbers to find out"
+      ),
+      React.createElement(
+        "h5",
+        null,
+        "Total Poops: ",
+        props.total
+      ),
+      React.createElement(ProxiedButton, { action: props.drink, label: "poop!" })
+    )
+  );
+};
+
+/////End main functionality
+var RegisterLogin = function RegisterLogin(props) {
+  var fields = React.createElement(
+    Paper,
+    { paperPadding: "0px", colMaxWidth: "300px" },
+    React.createElement(
+      MenuBar,
+      null,
+      React.createElement(
+        MenuText,
+        null,
+        "Please log in"
+      )
+    ),
+    React.createElement(
+      Form,
+      null,
+      React.createElement(
+        "div",
+        null,
+        React.createElement(
+          "label",
+          null,
+          React.createElement(
+            FormLabel,
+            null,
+            "Email:"
+          ),
+          React.createElement(
+            "div",
+            { className: "hintError" },
+            props.showHintEmail && React.createElement(
+              MouseText,
+              null,
+              "Please use a valid email"
+            ),
+            React.createElement(InputField, { type: "text", name: "email", onChange: props.action, value: props.email, required: true, pattern: "/^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$/" })
+          )
+        )
+      ),
+      React.createElement(
+        "div",
+        null,
+        React.createElement(
+          "label",
+          null,
+          React.createElement(
+            FormLabel,
+            null,
+            "Password:"
+          ),
+          React.createElement(
+            "div",
+            { className: "hintError" },
+            props.showHintPassword && React.createElement(
+              MouseText,
+              null,
+              "Minimum 6 characters"
+            ),
+            React.createElement(InputField, { type: "password", name: "password", onChange: props.action, value: props.password, required: true, pattern: ".{6,}" })
+          )
+        )
+      ),
+      React.createElement(
+        Row,
+        { rowJustify: "space-between" },
+        React.createElement(
+          SubmitButton,
+          { onClick: props.submit, type: "submit" },
+          "Register"
+        ),
+        React.createElement(
+          SubmitButton,
+          { onClick: props.logIn, type: "submit" },
+          "Login"
+        )
+      )
+    )
+  );
+
+  return React.createElement(
+    "div",
+    null,
+    fields
+  );
+};
+
+//stateful componenet
+//tracks the authentication state (Boolean)
+
+var AuthState = function (_React$Component2) {
+  _inherits(AuthState, _React$Component2);
+
+  function AuthState(props) {
+    _classCallCheck(this, AuthState);
+
+    ///set the default state
+
+    var _this4 = _possibleConstructorReturn(this, _React$Component2.call(this, props));
+
+    _this4.state = {
+      authenticated: true,
+      name: "",
+      errors: {}
+
+    };
+    return _this4;
+  }
+
+  ///component lifecycle
+
+  AuthState.prototype.componentWillMount = function componentWillMount() {
+
+    this.initAuthentication();
+  };
+
+  // step 1. start listening to firebase authentication
+
+  AuthState.prototype.initAuthentication = function initAuthentication() {
+    var _this5 = this;
+
+    console.log("Start listening to Firebase auth");
+    firebase.auth().onAuthStateChanged(function (user) {
+      console.log("Auth state has changed!!");
+      if (user) {
+        console.log("Person is authenticated");
+        _this5.setState({
+          authenticated: true,
+          email: null,
+          password: null
+        });
+      } else {
+        console.log("Person is NOT authenticated");
+        _this5.setState({
+          authenticated: false
+        });
+      }
+    });
+  };
+
+  ///anytime the user is typing
+
+  AuthState.prototype.onValueChange = function onValueChange(event) {
+    var _setState;
+
+    this.setState((_setState = {}, _setState[event.target.name] = event.target.value, _setState));
+    var validity = event.target.validity;
+    console.log(validity);
+    if (!validity.valid) {
+      var _errors;
+
+      this.setState({
+        errors: (_errors = {}, _errors[event.target.name + "Error"] = true, _errors)
+      });
+    } else {
+      var _errors2;
+
+      this.setState({
+        errors: (_errors2 = {}, _errors2[event.target.name + "Error"] = false, _errors2)
+      });
+    }
+  };
+
+  AuthState.prototype.onSubmit = function onSubmit(evt) {
+    evt.preventDefault();
+    this.register();
+  };
+
+  //2. register a new user
+
+  AuthState.prototype.register = function register() {
+    firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then(function () {
+      ///if it succeeds
+      console.log("it succeeded!!!");
+    }).catch(function (err) {
+      console.log("It failed ");
+      console.log(err);
+      alert(err.message);
+    });
+  };
+
+  //3. login a registered user
+
+  AuthState.prototype.login = function login(evt) {
+    evt.preventDefault();
+    console.log("log the user in");
+    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password).then(function () {
+      console.log("login succeeded");
+    }).catch(function (err) {
+      console.log("Failed login");
+      console.log(err);
+      alert(err.message);
+    });
+  };
+
+  //4. logout a registered user
+
+  AuthState.prototype.logout = function logout() {
+    firebase.auth().signOut();
+  };
+
+  AuthState.prototype.renderChildren = function renderChildren(children) {
+    var _this6 = this;
+
+    return React.Children.map(children, function (child) {
+      if (child.props.authenticationRequired && !_this6.state.authenticated) {
+        return React.createElement(RegisterLogin, { authenticated: _this6.state.authenticated, name: _this6.state.name, email: _this6.state.email, action: _this6.onValueChange.bind(_this6), password: _this6.state.password, submit: _this6.onSubmit.bind(_this6), logOut: _this6.logout.bind(_this6), logIn: _this6.login.bind(_this6), showHintEmail: _this6.state.errors.emailError, showHintPassword: _this6.state.errors.passwordError });
+      }
+      var cloned = React.cloneElement(child, {
+        authenticated: _this6.state.authenticated,
+        logOut: _this6.logout.bind(_this6)
+      });
+      return cloned;
+    });
+  };
+
+  AuthState.prototype.render = function render() {
+    return React.createElement(
+      "div",
+      null,
+      this.renderChildren(this.props.children)
+    );
+  };
+
+  return AuthState;
+}(React.Component);
+
+var Greeting = function (_React$Component3) {
+  _inherits(Greeting, _React$Component3);
+
+  function Greeting() {
+    _classCallCheck(this, Greeting);
+
+    return _possibleConstructorReturn(this, _React$Component3.call(this));
+  }
+
+  Greeting.prototype.render = function render() {
+    return React.createElement(
+      "div",
+      null,
+      React.createElement(
+        "button",
+        { onClick: this.props.logOut },
+        "Log Out"
+      ),
+      React.createElement(PoopData, null)
+    );
+  };
+
+  return Greeting;
+}(React.Component);
+
+var AuthenticationProvider = function AuthenticationProvider(props) {
+  return React.createElement(
+    AuthState,
+    null,
+    React.createElement(Greeting, { authenticationRequired: true })
+  );
+};
+ReactDOM.render(React.createElement(AuthenticationProvider, null), document.querySelector('#app'));
